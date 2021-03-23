4bffa08bddc95764aa4f940f15037fd5bb20eb0d
sipeiouyang <985491062@qq.com>
2021-03-23 22:27:23 +0800

    快速排序

---
+++ src/test/java/com/sipeiouyang/sort/QuickSortTest.java
@@ -0,0 +1,22 @@
+package com.sipeiouyang.sort;
+
+import com.sipeiouyang.LogicTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author sipeiouyang
+ * @version 1.0
+ */
+public class QuickSortTest extends LogicTest {
+
+    @Test
+    public void test(){
+        int[] arr = {9,0,2,3,4,1,7,6,8,5};
+        QuickSort.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            Assert.assertEquals(i, arr[i]);
+        }
+    }
+
+}
